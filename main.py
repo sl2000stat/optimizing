6707cbf96febe7697bc72b47957a1d1dbb54e256
sl2000stat <simon.leiner1@gmail.com>
2021-11-09 13:29:37 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+"""
+Name: main.py in Project: optimizing
+Author: Simon Leiner
+Date: 09.11.21
+Description: executable file in the project
+"""
+
+import pandas as pd
+import numpy as np
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Checked: Function works
+
+def execute_function():
+    """
+
+    This function executes the script
+
+    :return: None
+
+    """
+
+    return None
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    execute_function()
